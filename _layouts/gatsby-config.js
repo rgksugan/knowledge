9b56ac4aa8140e5f1b4d187bda7ac5b88a7beb40
Sugan G <rgk.sugan@gmail.com>
2023-02-05 12:08:58 +0530

    Rearrnge files

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
     {
       resolve: `gatsby-theme-kb`,
       options: {
-        rootNote: '/readme',
+        rootNote: '/index',
         contentPath: `${__dirname}/..`,
         ignore: [
           '**/_layouts/**',
