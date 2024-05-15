f22c601707a3bb4cb3cd3da8b8620742533df60a
Sugan G <rgk.sugan@gmail.com>
2024-05-15 13:29:29 +0530

    Add email workflow

---
+++ _email/src/highlight.ts
@@ -0,0 +1,50 @@
+import { PathLike } from "fs";
+import { readFile } from "fs/promises";
+import { Highlight } from "./types";
+
+const generateHighlightObject = (highlightData: string[]): Highlight => {
+    return {
+        bookTitle: highlightData[0]!,
+        content: highlightData[2]!,
+        details: highlightData[1]?.slice(2)!, // removes '- ' from the beginning.
+    };
+};
+
+export const highlightsParser = async (
+    path: PathLike
+): Promise<Highlight[]> => {
+    const content = await readFile(path, "utf-8");
+    const highlightsTexts = content.split("==========");
+
+    const filteredHighlights: Highlight[] = highlightsTexts
+        .map((highlightText) => {
+            // [bookTitle, details, content]
+            const highlightData: string[] = highlightText
+                .split("\r\n")
+                .join("\n") // To replace all `\r\n` with `\n`. Issue with ubuntu vs win
+                .split("\n")
+                .filter((f) => f.length > 0);
+
+            if (highlightData.length != 3) {
+                return null;
+            }
+
+            const highlight: Highlight = generateHighlightObject(highlightData);
+
+            return highlight;
+        })
+        .filter(Boolean) as Highlight[];
+
+    return filteredHighlights;
+};
+
+export const randomHighlightsSlice = (
+    highlights: Highlight[],
+    amount: number
+): Highlight[] => {
+    let slice = highlights
+        .sort(() => 0.5 - Math.random())
+        .slice(0, Math.min(amount, highlights.length));
+
+    return slice;
+};
---
+++ _email/src/index.ts
@@ -0,0 +1,19 @@
+import path from "path";
+import { NO_OF_HIGHLIGHTS } from "./env";
+import { email } from "./mail";
+import { highlightsParser, randomHighlightsSlice } from "./highlights";
+
+const run = async () => {
+    const clippingsPath = path.join("My Clippings.txt");
+
+    const allHighlights = await highlightsParser(clippingsPath);
+
+    const randomSlice = randomHighlightsSlice(
+        allHighlights,
+        NO_OF_HIGHLIGHTS >= 0 ? NO_OF_HIGHLIGHTS : 5
+    );
+
+    await email(randomSlice);
+};
+
+run();
